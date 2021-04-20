0aa5a60d5578e1a9a577a39c6e87d68cbd3e54a4
raaynaldo <raaynaldo@hotmail.com>
2021-04-20 12:11:17 -0400

    navbar component half way

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import styles from './Navbar.module.css';
 
 function Navbar() {
     return (
-        <header>
-            <div>
+        <header className={styles.wrapper}>
+            <div className={styles.navbar}>
                 <a href=''>
                     <h3>The Garden</h3>
                 </a>
                 <nav>
-                    <a href=''>Explore Garden</a>
-                    <a href=''>Log out</a>
+                    <a className={styles.navbar__link} href=''>
+                        Explore Garden
+                    </a>
+                    <a className={styles.navbar__link} href=''>
+                        Log out
+                    </a>
                 </nav>
             </div>
         </header>
--- pages/home.js
+++ pages/home.js
@@ -2,20 +2,24 @@ import React from 'react';
 import Goals from '@/components/Goals';
 import Prompt from '@/components/Prompt';
 import styles from '@/styles/Home.module.css';
+import Navbar from '@/components/Navbar/Navbar';
 
 const home = () => {
     return (
-        <div>
-            <h1>Welcome Home</h1>
-            <div styles={styles.container}>
-                <div className={styles.card}>
-                    <Goals />
-                </div>
-                <div className={styles.card}>
-                    <Prompt />
+        <>
+            <Navbar />
+            <div>
+                <h1>Welcome Home</h1>
+                <div styles={styles.container}>
+                    <div className={styles.card}>
+                        <Goals />
+                    </div>
+                    <div className={styles.card}>
+                        <Prompt />
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
