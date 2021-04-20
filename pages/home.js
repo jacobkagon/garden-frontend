45136a66d68f1269dcaa94d99131b049226dfd95
raaynaldo <raaynaldo@hotmail.com>
2021-04-20 10:09:39 -0400

    update link import using absolute import

--- pages/home.js
+++ pages/home.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import Goals from "../components/Goals"
-import Prompt from "../components/Prompt"
-import styles from '../styles/Home.module.css'
+import Goals from '@/components/Goals';
+import Prompt from '@/components/Prompt';
+import styles from '@/styles/Home.module.css';
 
 const home = () => {
     return (
         <div>
             <h1>Welcome Home</h1>
             <div styles={styles.container}>
-            <div className={styles.card}>
-            <Goals />
-            </div>
-            <div className={styles.card}>
-            <Prompt/>
-            </div> 
+                <div className={styles.card}>
+                    <Goals />
+                </div>
+                <div className={styles.card}>
+                    <Prompt />
+                </div>
             </div>
         </div>
     );
-}
+};
 
 export default home;
