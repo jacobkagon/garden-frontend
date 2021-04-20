009617ff48102fb3ab2f262006d99176fe40bc9f
JakesProjects1 <jacob@kagon.net>
2021-04-19 18:06:53 -0700

    added color to home container

--- components/Prompt.js
+++ components/Prompt.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const Prompt = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  return (
+    <div>
+      <h3>Prompt Answers</h3>
+      <div></div>
+    </div>
+  );
+};
 
 export default Prompt;
