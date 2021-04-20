d82d7c49f265604ee44a55f783ef72e3050d9a1d
purisquiri <mau.dorigo@outlook.es>
2021-04-20 12:15:35 -0400

    welcome page done

--- components/Welcome/Welcome.js
+++ components/Welcome/Welcome.js
@@ -2,13 +2,17 @@ import React from 'react';
 import styles from './Welcome.module.css';
 
 const Welcome = () => {
-  return (
-    <div>
-      <h1 className={styles.header}>The Garden</h1>
-      <hr />
-      <p>A place to discover and explore our differences</p>
-    </div>
-  );
+    return (
+        <>
+            <div>
+                <h1 className={styles.header}>The Garden</h1>
+
+                <p className={styles.slogan}>
+                    A place to discover and explore our differences
+                </p>
+            </div>
+        </>
+    );
 };
 
 export default Welcome;
