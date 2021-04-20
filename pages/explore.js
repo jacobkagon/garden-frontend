fa230f8918e104f3fd71d58230289600e8596ea9
njerimk <nmurugikamau@gmail.com>
2021-04-20 10:55:03 -0400

    updated package and added explore and workbook pages

---
+++ pages/explore.js
@@ -0,0 +1,9 @@
+import React from 'react'
+export const explore = () => {
+    return (
+        <div>
+            <h1>Explore</h1>
+        </div>
+    )
+}
+export default explore
---
+++ pages/workbook.js
@@ -0,0 +1,9 @@
+export const workbook = () => {
+    return (
+        <div>
+            <h1>Workbook</h1>
+        </div>
+    )
+}
+
+export default workbook
