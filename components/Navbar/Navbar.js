75c8528a872d35dc3d88889dcb3a19b5ba996876
raaynaldo <raaynaldo@hotmail.com>
2021-04-20 11:26:31 -0400

    navbar file

---
+++ components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <header>
+            <div>
+                <a href=''>
+                    <h3>The Garden</h3>
+                </a>
+                <nav>
+                    <a href=''>Explore Garden</a>
+                    <a href=''>Log out</a>
+                </nav>
+            </div>
+        </header>
+    );
+}
+
+export default Navbar;
