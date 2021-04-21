055ac348f6c3649fb969ee48b32990f42e3222b1
chirptotheeast <alyssa.easterly@yahoo.com>
2021-04-21 14:42:56 -0400

    created files for workbout component

---
+++ components/Workbook/Workbook.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './Workbook.module.css';
+
+const Workbook = () => {
+    return (
+        <>
+            <div className={styles.header}>
+                <h1>The Garden</h1>
+
+                <p className={styles.slogan}>
+                    A place to discover and explore our differences
+                </p>
+            </div>
+        </>
+    );
+};
+
+export default Welcome;
