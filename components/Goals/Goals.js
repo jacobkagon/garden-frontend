9ea705aee09c83046a2bd65fa7c274a61f175624
purisquiri <mau.dorigo@outlook.es>
2021-04-21 17:38:52 -0400

    homepaga has its own component folder

--- components/Goals/Goals.js
+++ components/Goals/Goals.js
@@ -2,15 +2,12 @@ import React from 'react';
 import styles from './Goals.module.css';
 const Goals = () => {
     return (
-        <div>
-            <div className={styles.card}>
-                <div className={styles.title}>Goals</div>
-                <div className={styles.cardHome}>
-                    <span className={styles.goalsList}>
-                        <a href='/explore'>this is a hiperlink</a>
-                    </span>
-
-                </div>
+        <div className={styles.card}>
+            <div className={styles.title}>Goals</div>
+            <div className={styles.cardHome}>
+                <span className={styles.goalsList}>
+                    <a href='/explore'>this is a hiperlink</a>
+                </span>
             </div>
         </div>
     );
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import Head from 'next/head';
 import styles from '@/styles/Home.module.css';
 import Welcome from '@/components/Welcome/Welcome';
 import Navbar from '@/components/Navbar/Navbar';
+import Homepage from '@/components/Home/Home';
 
 export default function Home() {
     return (
         <div>
             <Navbar />
-            <Welcome />
+            {/* <Welcome /> */}
+            <Homepage />
         </div>
     );
 }
