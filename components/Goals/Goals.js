5d9e6198d536ec39cd57c32183961625868a198e
purisquiri <mau.dorigo@outlook.es>
2021-04-21 12:45:30 -0400

    home page goal with hyperlynk

---
+++ components/Goals/Goals.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import styles from './Goals.module.css';
+const Goals = () => {
+    return (
+        <div>
+            <div className={styles.card}>
+                <div className={styles.title}>Goals</div>
+                <div className={styles.cardHome}>
+                    <span className={styles.goalsList}>
+                        this is my first goal of this beautiful jounal
+                    </span>
+                    <span className={styles.goalsList}>
+                        this is my first goal of this beautiful jounal
+                    </span>
+                    <span className={styles.goalsList}>
+                        <a href='/explore'>this is a hiperlink</a>
+                    </span>
+                    <span className={styles.goalsList}>
+                        this is my first goal of this beautiful jounal dhdbhd
+                        dhdbd
+                    </span>
+                    <span className={styles.goalsList}>
+                        this is my first goal of this beautiful jounal
+                    </span>
+                    <span className={styles.goalsList}>
+                        this is my first goal of this beautiful jounal dededwb
+                        edwedwd wedwedewd edewdewdw ewdewdewed wedwededwe edwede
+                    </span>
+                    <span className={styles.goalsList}>
+                        this is my first goal of this beautiful jounal
+                    </span>
+                    <span className={styles.goalsList}>
+                        this is my first goal of this beautiful jounal
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Goals;
