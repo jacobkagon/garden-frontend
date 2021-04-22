c134f557749188cc42881e7fcc48b8b6adef759c
raaynaldo <raaynaldo@hotmail.com>
2021-04-21 23:40:48 -0400

    create axios deploable to server without change the url.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css';
 import { AuthProvider } from '@/lib/auth';
+import axios from 'axios';
 
 function MyApp({ Component, pageProps }) {
+    if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+        // dev code
+        axios.defaults.baseURL = `http://localhost:3001/api/v1`;
+    } else {
+        // production code
+        axios.defaults.baseURL = `https://jot-down-api.herokuapp.com/api/v1`;
+    }
+
     return (
         <AuthProvider>
             <Component {...pageProps} />
