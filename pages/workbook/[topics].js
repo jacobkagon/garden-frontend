2dacc07af01e120fe7a5b47d4b7ad038c5dbd7b9
JakesProjects1 <jacob@kagon.net>
2021-04-22 16:32:25 -0400

    added random questions

--- pages/workbook/[topics].js
+++ pages/workbook/[topics].js
@@ -1,11 +1,13 @@
 import Workbook from '@/components/Workbook/Workbook.js';
 import Navbar from '@/components/Navbar/Navbar';
+import { useRouter } from 'next/router'
 
 export const workbook = () => {
+    const router = useRouter()
     return (
         <div>
             <Navbar/>
-            <Workbook/>
+            <Workbook number={router.query.topics}/>
         </div>
     );
 };
