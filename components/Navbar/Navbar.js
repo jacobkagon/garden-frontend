fade5728258e95d5ae2fcb888e1bf3821c6913d3
chirptotheeast <alyssa.easterly@yahoo.com>
2021-04-25 18:11:16 -0400

    updated favicon

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -10,51 +10,57 @@ function Navbar() {
 
     return (
         <header className={styles.wrapper}>
-            <div className={styles.navbar}>
-                <Link href='/'>
-                    <a style={{ display: 'flex', alignItems: 'flex-end' }}>
-                        <h2>The Garden</h2>
-                        <Image
-                           className={styles.image}
-                            width={45}
-                            height={60}
-                            // responsive={true}
-                            src='/images/icons/potstem.PNG'
-                            alt='pot'
-                        />
-                    </a>
-                </Link>
+            <div style={{ display: 'flex', alignItems: 'flex-end' }}>
+                <div className={styles.navbar}>
+                    <Link href='/'>
+                        <a style={{ display: 'flex', alignItems: 'flex-end' }}>
+                            <h2>The Garden</h2>
+                            <Image
+                                className={styles.image}
+                                width={45}
+                                height={60}
+                                // responsive={true}
+                                src='/images/icons/potstem.PNG'
+                                alt='pot'
+                            />
+                        </a>
+                    </Link>
 
-                {auth.user ? (
-                    <nav className={styles.nav_wrap_right}>
-                        <Link href='/explore'>
-                            <a
+                    {auth.user ? (
+                        <div className={styles.nav_right_wrappy}
+                            // style={{ display: 'flex', alignItems: 'flex-end', flexDirection: 'row'}}
+                        >
+                            <nav >
+                                <Link href='/explore'>
+                                    <a
+                                        className={styles.navbar__link}
+                                        className={styles.un}
+                                        // className={styles.nav_wrap_right}
+                                    >
+                                        Explore Garden
+                                    </a>
+                                </Link>
+                                <button
+                                    className={styles.navbar__link}
+                                    className={styles.un}
+                                    onClick={() => auth.signout()}
+                                    // className={styles.nav_wrap_right}
+                                >
+                                    Sign Out
+                                </button>
+                            </nav>
+                        </div>
+                    ) : (
+                        <nav>
+                            <button
                                 className={styles.navbar__link}
-                                className={styles.un}
-                                // className={styles.nav_wrap_right}
+                                onClick={() => auth.signinWithGoogle()}
                             >
-                                Explore Garden
-                            </a>
-                        </Link>
-                        <button
-                            className={styles.navbar__link}
-                            className={styles.un}
-                            onClick={() => auth.signout()}
-                            // className={styles.nav_wrap_right}
-                        >
-                            Sign Out
-                        </button>
-                    </nav>
-                ) : (
-                    <nav>
-                        <button
-                            className={styles.navbar__link}
-                            onClick={() => auth.signinWithGoogle()}
-                        >
-                            Sign In With Google
-                        </button>
-                    </nav>
-                )}
+                                Sign In With Google
+                            </button>
+                        </nav>
+                    )}
+                </div>
             </div>
         </header>
     );
