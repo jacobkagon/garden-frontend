cceba530bcb3f92fb88c35d785ff2d5c063bbb13
purisquiri <mau.dorigo@outlook.es>
2021-04-24 22:02:24 -0400

    fix some bugs in prompt modal

--- components/Prompt/Prompt.js
+++ components/Prompt/Prompt.js
@@ -1,16 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import styles from './Prompt.module.css';
 import axios from 'axios';
-// import ReadMoreReact from 'read-more-react';
-import { Modal, useModal, ModalTransition } from 'react-simple-hook-modal';
-import 'react-simple-hook-modal/dist/styles.css';
-import { ImCross } from 'react-icons/im';
+import PromptModal from './PromptModal/PromptModal';
 
 const Prompt = () => {
     const [answers, setAnswers] = useState([]);
-    // const [modal, setModal] = useState(false);    // let theDate = new Date(answers.created_at);
-    const { isModalOpen, openModal, closeModal } = useModal();
-    // let dateString = theDate.toDateString();
 
     useEffect(() => {
         fetchData();
@@ -28,11 +22,6 @@ const Prompt = () => {
             });
     };
 
-    // function callModal() {
-    //      <GoalModal/>
-
-    // }
-
     console.log(answers);
     return (
         <div className={styles.wrapper}>
@@ -46,38 +35,14 @@ const Prompt = () => {
                                 ?.split(' ')
                                 .slice(0, 8)
                                 .join(' ')}
+                            ...
                         </div>
                         <div className={styles.promptAnswer}>
                             {answer.answer?.split(' ').slice(0, 10).join(' ')}
+                            ...
                         </div>
-                        <button onClick={openModal}>Read More</button>
-                        <Modal
-                            id='goals_or_any_id'
-                            isOpen={isModalOpen}
-                            transition={ModalTransition.NONE}
-                        >
-                            <button
-                                onClick={closeModal}
-                                className={styles.btnClose}
-                            >
-                                <ImCross />
-                            </button>
-                            <div className={styles.promptQuestion}>
-                                {answer.question_prompt.question}
-                            </div>
-                            <div className={styles.promptAnswer}>
-                                {answer.answer}
-                            </div>
-                            <div className={styles.promptFooter}>
-                                <div style={{ textAlign: 'right' }}>
-                                    {answer.question_prompt.topic}
-                                </div>
-                                <div style={{ textAlign: 'right' }}>
-                                    {new Date(answer.created_at).toDateString()}
-                                </div>
-                            </div>
-                        </Modal>
-                        {/* <button onClick={() => answer.answer}>Read More...</button> */}
+
+                        <PromptModal answer={answer} />
                         <div className={styles.promptFooter}>
                             <div style={{ textAlign: 'right' }}>
                                 {answer.question_prompt.topic}
