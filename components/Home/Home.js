3a14f376eb653edd3bd2c56c489a538b4b8b5d81
JakesProjects1 <jacob@kagon.net>
2021-04-25 13:42:07 -0400

    added date

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -8,12 +8,25 @@ import { useAuth } from '@/lib/auth';
 const Home = () => {
     const auth = useAuth();
 
+    const curday = (sp) =>{
+        let today = new Date();
+        let dd = today.getDate();
+        let mm = today.getMonth()+1; //As January is 0.
+        let yyyy = today.getFullYear();
+        
+        if(dd<10) dd='0'+dd;
+        if(mm<10) mm='0'+mm;
+        return (mm+sp+dd+sp+yyyy);
+        };
+
     return (
         <>
             <div className={styles.wrapper}>
                 <h1 className={styles.title}>
                     Welcome, {auth.user.name.split(' ')[0]}
+                   
                 </h1>
+                <div className={styles.title}>{curday('/')}</div>
                 <div className={styles.container}>
                     <Goals />
                     <img
